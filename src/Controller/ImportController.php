7ed8f231349df83e2e35c8603616a7731390703c
Florian Berrotµ <florian.berrot@ynov.com>
2021-02-19 09:21:57 +0100

    add controller

---
+++ src/Controller/ImportController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ImportController extends AbstractController
+{
+    #[Route('/import', name: 'import')]
+    public function index(): Response
+    {
+        return $this->render('import/index.html.twig', [
+            'controller_name' => 'ImportController',
+        ]);
+    }
+}
